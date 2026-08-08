012b5f2a2d50f48ec87c1f0f40638fb3839dd07b
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-08-09 00:01:36 +0430

    create options component for each question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,9 @@
-import Options from './Options';
-export default function Question({question}){
-    return <div>
-    <h4>{question.question}</h4>
-       <Options question={question }/>
+import Options from "./Options";
+export default function Question({ question, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
+  );
 }
