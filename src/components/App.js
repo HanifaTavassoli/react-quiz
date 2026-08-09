d86ce37a478c046573e907672eb172ebd3f68613
Hanifa Tavassoli <hanifatavassoli@gmail.com>
2026-08-09 12:33:45 +0430

    add finishScreen with its functionality

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 
 const initialState = {
@@ -18,6 +19,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore:0
 };
 
 const reducer = (state, action) => {
@@ -51,13 +53,16 @@ const reducer = (state, action) => {
     case "nextQuestion":
       return {...state, index:state.index + 1, answer: null}  
 
+    case "finish":
+      return {...state, status: "finished",
+         highscore:state.points > state.highscore ? state.points : state.highscore}
     default:
       throw new Error("Action unkown");
   }
 };
 
 function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points,highscore }, dispatch] = useReducer(
     reducer,
     initialState,
   );
@@ -94,9 +99,25 @@ function App() {
             answer={answer}
             dispatch={dispatch}
             />
-           <NextButton dispatch={dispatch} answer={answer}/> 
+           <NextButton 
+           dispatch={dispatch}
+           answer={answer}
+           index={index}
+           numQuestions={numQuestions}
+          /> 
           </>
         )}
+        {
+          status === "finished" && (
+            <FinishScreen 
+            points={points} 
+            maxPossiblePoints={maxPossiblePoints} 
+            index={index}
+            numQuestions={numQuestions}
+            highscore={highscore}
+            />
+          )
+        }
       </Main>
     </div>
   );
